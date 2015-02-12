55ce33d99c878dbe913953798c36d758cc8207d0
Cathleen Carbonell <catcarbonell@gmail.com>
2015-02-11 22:27:52 -0800

    Wk 2 -- Implemented tooltip and updated jQuery lib

--- Carbonell_Cathleen_Project/js/main.js
+++ Carbonell_Cathleen_Project/js/main.js
@@ -1,14 +1,26 @@
 /*  
-	Your Project Title
-	Author: You
+	ATK PLAN: A Productivity Application!
+	Author: Cathleen Carbonell
 */
 
 (function($){
-	
-	
-	
-	
-		
+	/* ======= TOOL TIP ======= */
+    //$(document).ready(function(){
+        $('.masterToolTip').hover(function(){
+        //Hover code
+        var title= $(this).attr('title');
+        $(this).data('tipText', title).removeAttr('title');
+        $('<p class="tooltip"></p>').text(title).appendTo('body').fadeIn('slow');
+    }, function() {
+        //Hover out
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+    }).mousemove(function(e) {
+        var mousex = e.pageX + 20; //X coordinates
+        var mousey = e.pageY + 10; //Y coordinates
+        $('.tooltip').css({ top: mousey, left: mousex })
+    });
+//});
 
 	
 })(jQuery); // end private scope
