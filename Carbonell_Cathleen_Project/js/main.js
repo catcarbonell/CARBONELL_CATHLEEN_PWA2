eff897e451ce37d8ed8b493f99184ae618609ab0
Cathleen Carbonell <catcarbonell@gmail.com>
2015-02-12 14:34:51 -0800

    Wk 2 -- Tabbed Accordion
    Tried code, will fix for functionality

--- Carbonell_Cathleen_Project/js/main.js
+++ Carbonell_Cathleen_Project/js/main.js
@@ -23,6 +23,21 @@
         $('.tooltip').css({ top: mousey, left: mousex })
     });
 
+    /* ======= TABBED ACCORDION ======= */
+
+    $('#tabs p').hide().eq(0).show();
+    $('#tabs p:not(:first)').hide();
+
+    $('.tabnav li').click(function(e) {
+        e.preventDefault();
+        $('#tabs p').hide();
+
+    $('.tabnav .current').removeClass("current");
+        $(this).addClass('current');
+        var clicked = $(this).find('a:first').attr('href');
+
+        $('#tabs ' + clicked).fadeIn('fast');
+     }).eq(0).addClass('current');
 //});
 
 	
