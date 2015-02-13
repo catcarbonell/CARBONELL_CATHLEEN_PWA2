784067b8e1f71e9f6fc6699d80a2e083043e8c54
Cathleen Carbonell <catcarbonell@gmail.com>
2015-02-13 14:01:46 -0800

    Wk 2 -- Modal added

--- Carbonell_Cathleen_Project/js/main.js
+++ Carbonell_Cathleen_Project/js/main.js
@@ -5,40 +5,97 @@
 
 (function($){
     //$(document).ready(function(){
-	/* ======= TOOL TIP ======= */
+    /* ======= TOOL TIP ======= */
 
-
-        $('.masterToolTip').hover(function(){
+    $('.masterToolTip').hover(function(){
         //Hover code
         var title= $(this).attr('title');
         $(this).data('tipText', title).removeAttr('title');
         $('<p class="tooltip"></p>').text(title).appendTo('body').fadeIn('slow');
-    }, function() {
+   }, function() {
         //Hover out
         $(this).attr('title', $(this).data('tipText'));
         $('.tooltip').remove();
-    }).mousemove(function(e) {
+   }).mousemove(function(e) {
         var mousex = e.pageX + 20; //X coordinates
         var mousey = e.pageY + 10; //Y coordinates
         $('.tooltip').css({ top: mousey, left: mousex })
-    });
+   });
 
-    /* ======= TABBED ACCORDION ======= */
+   /* ======= TABBED ACCORDION ======= */
 
-    $('#tabs p').hide().eq(0).show();
+   /* $('#tabs p').hide().eq(0).show();
     $('#tabs p:not(:first)').hide();
 
-    $('.tabnav li').click(function(e) {
+    $('#tabsnav li').click(function(e) {
+        //$('#projects').hide();
         e.preventDefault();
         $('#tabs p').hide();
 
-    $('.tabnav .current').removeClass("current");
+        $('#tabsnav .current').removeClass("current");
         $(this).addClass('current');
         var clicked = $(this).find('a:first').attr('href');
 
         $('#tabs ' + clicked).fadeIn('fast');
-     }).eq(0).addClass('current');
-//});
+    }).eq(0).addClass('current'); */
+
+   /* $('div.info').hide();
+   $('li.title').click(function() {
+        $('div.info').toggleClass('hide').slideToggle('fast');
+    });
+    });*/
+
+   $('ul.tabs').each(function(){
+       // For each set of tabs, we want to keep track of
+       // which tab is active and it's associated content
+       var $active, $content, $links = $(this).find('a');
+
+        // If the location.hash matches one of the links, use that as the active tab.
+        // If no match is found, use the first link as the initial active tab.
+        $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+        $active.addClass('active');
+
+        $content = $($active[0].hash);
+
+        // Hide the remaining content
+        $links.not($active).each(function () {
+            $(this.hash).hide();
+        });
+
+        // Bind the click event handler
+        $(this).on('click', 'a', function(e){
+            // Make the old tab inactive.
+            $active.removeClass('active');
+            $content.hide();
+
+            // Update the variables with the new link and content
+            $active = $(this);
+            $content = $(this.hash);
+
+            // Make the tab active.
+            $active.addClass('active');
+            $content.show();
+
+            // Prevent the anchor's default click action
+            e.preventDefault();
+        });
+    });
+
+   /* ======= MODAL POPUP ======= */
+
+   // Adds Modal
+
+    $('#overlay').hide();
+    $('a.modalClick').click(function(e) {
+        e.preventDefault();
+        $('#overlay').toggleClass('hide').fadeToggle('fast');
+    });
+
+    // Closes Modal
+   $('.close').on('click', function(e){
+        e.preventDefault();
+        $('#overlay').toggleClass('hide').fadeOut('fast');
+   });
 
 	
 })(jQuery); // end private scope
