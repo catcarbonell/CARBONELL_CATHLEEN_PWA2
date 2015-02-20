a04b45dc0dae9554b2cc67d7f21b7668d7f67de5
Cathleen Carbonell <catcarbonell@gmail.com>
2015-02-20 02:22:47 -0800

    Wk3 -- Project -- Implemented JSON/AJAX components

--- Carbonell_Cathleen_Project/js/main.js
+++ Carbonell_Cathleen_Project/js/main.js
@@ -4,6 +4,7 @@
 */
 
 (function($){
+
     //$(document).ready(function(){
 
     /* ======= TOOL TIP ======= */
@@ -89,6 +90,98 @@
         });
     });
 
+    /* ======= ADD PROJECT ======= */
+
+    $("#addButton").click(function(){
+       var projName = $('#projectName').val(),
+           projDesc = $('#projectDescription').val(),
+           projDue = $('#projectDueDate').val(),
+           status = $('input[name = "status"]:checked').prop("id");
+
+       $.ajax({
+            url : "xhr/new_project.php",
+            type : "post",
+            dataType : "json",
+            data : {
+                projectName : projName,
+                projectDescription : projDesc,
+                dueDate : projDue,
+                status : status
+            },
+
+            success : function(response){
+                if(response.error){
+                    alert(response.error);
+                }else{
+                    window.location.assign("projects.html");
+                }
+            }
+        })
+    });
+
+    /* ======= DISPLAY PROJECT ====== */
+
+    var projects = function(){
+
+        $.ajax({
+            url : "xhr/get_projects.php",
+            type : "get",
+            dataType : "json",
+            success : function(response){
+                if(response.error){
+                    alert(response.error);
+                }else{
+                    for(var i= 0, j=response.projects.length; i<j; i++){
+                        var result = response.projects[i];
+
+                    $('.projects').append(
+                        '<div class="projectbox">' + '<input class="projectid" name="projectid" id="projectid" type="hidden" value="' + result.id + '">'
+                        + "Project Name: " + result.projectName + '<br />' +
+                        "Project Description: " + result.projectDescription + '<br />' +
+                        "Project Status: " + result.status + '<p />' +
+
+                        '<button class="menubtn deletebtn">Delete</button>' + ' ' +
+                        '<button class="menubtn editbtn">Edit</button>' +
+                         '</div>' + '<br />'
+                    ); //Close append
+                } //Close If/Else
+
+                // DELETE BTN
+
+                $('.deletebtn').on('click', function(e){
+
+                    e.preventDefault();
+                    var projectid = $("#projectid").val();
+
+                    console.log('deleted.');
+
+                    $.ajax({
+                        url : "xhr/delete_project.php",
+                        data : {
+                            projectID: projectid //calls individual project id rather than last
+                        },
+                        type : "post",
+                        dataType: "json",
+                        success : function(response){
+                            if(response.error){
+                                alert(response.error);
+                            }else{
+                                window.location.assign("projects.html")
+                            } // Close Else
+                        } // Close success
+                    }); // Close ajax
+                }); // Close .deletebtn
+
+                } // Close projects for loop
+            } // Close response
+        }); // Close AJAX
+        return false
+    }; //Close projects fn
+
+    //projects();
+
+    if(/.+projects.htm.*/i.test(window.location.valueOf())){projects();}
+
     /* ======= NOTES SLIDESHOW ======= */
 
     $("#notes > div:gt(0)").hide();
@@ -126,8 +219,7 @@
             success : function(response) {
 
                 if(response.error){
-                    alert.('Login failed. Try again.');
-                    console.log('error')
+                    alert(response.error);
                 }else{
                     window.location.assign('dashboard.html');
                 }
@@ -137,7 +229,65 @@
 
     /* ======= LOGOUT ======= */
 
-    
+    $('#logout').click(function(e){
+        e.preventDefault();
+        $.get('xhr/logout.php', function(){
+            window.location.assign('index.html')
+        })
+    });
+
+    /* ======= DYNAMIC NAME DISPLAY ======= */
+
+    $.getJSON("xhr/check_login.php" , function(data){
+
+        $.each(data, function(key, val){
+            $(".userid").html('Welcome, ' + val.first_name);
+        })
+    });
+
+    /* ======= REGISTRATION ======= */
+
+    $('#register').click(function(e){
+        e.preventDefault();
+
+        var email = $('#email').val(),
+            firstname = $('#first').val(),
+            lastname = $('#last').val(),
+            username = $('#username').val(),
+            password = $('#password').val();
+
+            console.log(email + ' ' + firstname + ' ' + lastname + ' ' + username + ' ' + password);
+
+        $.ajax({
+            url : 'xhr/register.php',
+            type : 'post',
+            dataType : 'json',
+            data : {
+                email : email,
+                firstname : firstname,
+                lastname : lastname,
+                username : username,
+                password : password
+            },
+
+            success:function(response){
+
+                if(response.error){
+                    alert(response.error);
+                }else{
+                    console.log('hooray');
+                    window.location.assign('projects.html');
+                }
+
+            }
+
+        });
+
+    });
+
+
+
+
 
 })(jQuery); // end private scope
 
